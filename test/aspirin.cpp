90a5b1525717015ceaf59cf7be951a6ac1f35cae
Sam Kumar <samkumar99@gmail.com>
2020-05-26 20:07:43 +0000

    Fix bugs in aspirin count circuit

--- test/aspirin.cpp
+++ test/aspirin.cpp
@@ -8,25 +8,24 @@ struct input {
     Bit diagnosis; // or aspirin prescription
 };
 
-void aspirin(int input_size_per_party, int patient_id_bits = 32, int timestamp_bits = 32, int result_bits = 32) {
-    int input_array_length = input_size_per_party * 2;
+void aspirin(uint64_t input_size_per_party, int patient_id_bits = 32, int timestamp_bits = 32, int result_bits = 32) {
+    uint64_t input_array_length = input_size_per_party * 2;
     Integer* patient_id_concat_timestamp = new Integer[input_array_length];
     Bit* diagnosis = new Bit[input_array_length]; // true if entry at this index is a diagnosis, false if it's an aspirin prescription
-    struct input* inputs = new struct input[input_array_length];
-    for (int i = 0; i != input_array_length; i++) {
-        patient_id_concat_timestamp[i] = Integer(patient_id_bits + timestamp_bits, 0, i < input_size_per_party ? ALICE : ALICE);
-        Integer b(1, 0, i < input_size_per_party ? ALICE : ALICE); // for now
+    for (uint64_t i = 0; i != input_array_length; i++) {
+        patient_id_concat_timestamp[i] = Integer(patient_id_bits + timestamp_bits, i < input_size_per_party ? (((input_size_per_party - i - 1) << 32) | 1) : (((i - input_size_per_party) << 32) | 2), i < input_size_per_party ? ALICE : ALICE);
+        Integer b(1, (i < input_size_per_party - 1) ? 1 : 0, i < input_size_per_party ? ALICE : ALICE); // for now
         diagnosis[i] = b[0];
     }
 
     // Verify the input first.
     Bit verifyOrder(true);
-    for (int i = 0; i < input_size_per_party - 1; i++) {
-        Bit lessThanNext = !(patient_id_concat_timestamp[i] > patient_id_concat_timestamp[i+1]);
+    for (uint64_t i = 0; i < input_size_per_party - 1; i++) {
+        Bit lessThanNext = patient_id_concat_timestamp[i].geq(patient_id_concat_timestamp[i+1]);
         verifyOrder = verifyOrder & lessThanNext;
     }
-    for (int i = input_size_per_party; i < 2 * input_size_per_party - 1; i++) {
-        Bit greaterThanNext = patient_id_concat_timestamp[i].geq(patient_id_concat_timestamp[i+1]);
+    for (uint64_t i = input_size_per_party; i < 2 * input_size_per_party - 1; i++) {
+        Bit greaterThanNext = !(patient_id_concat_timestamp[i] > patient_id_concat_timestamp[i+1]);
         verifyOrder = verifyOrder & greaterThanNext;
     }
     bool sorted = verifyOrder.reveal<bool>(ALICE);
@@ -38,15 +37,14 @@ void aspirin(int input_size_per_party, int patient_id_bits = 32, int timestamp_b
     Integer one(result_bits, 1, PUBLIC);
 
     // Now, for each input, check if it and the next input have the same patient, but the first is a diagnosis and the second isn't.
-    for (int i = 0; i < input_array_length; i++) {
-        Bit add = inputs[i].diagnosis & !inputs[i+1].diagnosis;
-        add = add & ((inputs[i].patient_id_concat_timestamp >> timestamp_bits).equal(inputs[i+1].patient_id_concat_timestamp >> timestamp_bits));
+    for (uint64_t i = 0; i < input_array_length - 1; i++) {
+        Bit add = diagnosis[i] & !diagnosis[i + 1];
+        add = add & ((patient_id_concat_timestamp[i] >> timestamp_bits).equal(patient_id_concat_timestamp[i + 1] >> timestamp_bits));
         Integer next = total + one;
-        total.select(add, next);
+        total = total.select(add, next);
     }
 
     std::uint32_t result = total.reveal<std::uint32_t>(ALICE);
-    delete[] inputs;
 
     cout << sorted << endl;
     cout << result << endl;
@@ -65,7 +63,7 @@ int main(int argc, char** argv) {
 
     int port, party;
     parse_party_and_port(argv, &party, &port);
-    NetIO * io = new NetIO(party==ALICE ? nullptr : "127.0.0.1", port);
+    NetIO * io = new NetIO(party==ALICE ? nullptr : "10.0.0.246", port);
 
     setup_semi_honest(io, party);
 
